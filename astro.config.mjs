8b321799705827e7ef4c6e9c387ae9bf6043908e
QYNebula <19906100577@189.cn>
2026-01-07 20:38:36 +0800

    initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,11 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+    i18n: {
+    defaultLocale: 'fr',
+    locales: ['en', 'fr'],
+    routing: { prefixDefaultLocale: false }
+    }
+});
