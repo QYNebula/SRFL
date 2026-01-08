7f68c4e1280aca27ff8fd96185a344373b2cd2e8
QYNebula <19906100577@189.cn>
2026-01-08 17:06:25 +0800

    #

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
 export default defineConfig({
-    i18n: {
-    defaultLocale: 'fr',
-    locales: ['en', 'fr'],
-    routing: { prefixDefaultLocale: false }
-    }
+  i18n: {
+  defaultLocale: 'fr',
+  locales: ['en', 'fr'],
+  routing: { prefixDefaultLocale: false }
+  },
+
+  adapter: vercel()
 });
